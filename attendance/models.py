9f9d5e5573519f1e0e2535a8a8481667f51c2379
Tyson Paul <tyson.paul@digitalmesh.com>
2020-02-21 19:10:25 +0530

    dashboard

--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+class Attendance(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    punch_in = models.DateTimeField(auto_now=False, auto_now_add=False)
+    punch_out = models.DateTimeField(auto_now=False, auto_now_add=False)
